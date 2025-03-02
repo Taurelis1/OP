1e8f32471678482566c9c1c4d6374347dfae5f95
Tauras <tauras.petrauskas@mif.stud.vu.lt>
2025-03-02 18:11:50 +0200

    Mylib

--- main.cpp
+++ main.cpp
@@ -1,31 +1,4 @@
-﻿#include <iostream>
-#include <string>
-#include <iomanip>
-#include <algorithm>
-#include <vector>
-#include <limits>
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
-#include <sstream>
-#include <chrono>
-
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
-using std::fixed;
-using std::setprecision;
-using std::setw;
-using std::left;
-using std::vector;
-using std::ifstream;
-using std::ofstream;
-using std::istringstream;
-using std::chrono::high_resolution_clock;
-using std::chrono::duration_cast;
-using std::chrono::duration;
-using std::chrono::milliseconds;
+﻿#include "Mylib.h"
 
 struct Studentas {
     string var;
