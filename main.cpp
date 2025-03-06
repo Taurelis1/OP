b108ffae340b8fea035ef58d7a4ac2cfbbc8f7eb
Tauras <tauras.petrauskas@mif.stud.vu.lt>
2025-03-06 19:49:18 +0200

    max generuojamu studentu limitas padidintas iki 10000000 ir geresnis atminties valdymas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Studentas.h"
 #include "Mylib.h"
 
+const int MAX_STUDENTU_SKAICIUS = 10000000; // Nustatome maksimalu studentu skaiciu
+
 int main() {
     try {
         auto start = high_resolution_clock::now(); // Pradeti laikmati
@@ -20,22 +22,28 @@ int main() {
             return 0;
         } else if (pasirinkimas == 'g') {
             int studentuSkaicius;
-            cout << "Iveskite studentu skaiciu: ";
-            while (!(cin >> studentuSkaicius) || studentuSkaicius <= 0) {
+            cout << "Iveskite studentu skaiciu (maksimalus skaicius: " << MAX_STUDENTU_SKAICIUS << "): ";
+            while (!(cin >> studentuSkaicius) || studentuSkaicius <= 0 || studentuSkaicius > MAX_STUDENTU_SKAICIUS) {
                 clearInput();
                 cout << "Neteisinga ivestis. Bandykite dar karta: ";
             }
-            for (int i = 0; i < studentuSkaicius; i++) {
-                Studentas student;
-                student.var = generuotiVarda();
-                student.pav = generuotiPavarde();
-                int ndSkaicius = std::rand() % 10 + 1; // Generuojame nuo 1 iki 10 namu darbu
-                student.nd.resize(ndSkaicius);
-                for (int j = 0; j < ndSkaicius; j++) {
-                    student.nd[j] = std::rand() % 11; // Generuojame balus nuo 0 iki 10
+            try {
+                studentai.reserve(studentuSkaicius); // Rezervuojame atminties vieta
+                for (int i = 0; i < studentuSkaicius; i++) {
+                    Studentas student;
+                    student.var = generuotiVarda();
+                    student.pav = generuotiPavarde();
+                    int ndSkaicius = std::rand() % 10 + 1; // Generuojame nuo 1 iki 10 namu darbu
+                    student.nd.resize(ndSkaicius);
+                    for (int j = 0; j < ndSkaicius; j++) {
+                        student.nd[j] = std::rand() % 11; // Generuojame balus nuo 0 iki 10
+                    }
+                    student.egz = std::rand() % 11; // Generuojame egzamino bala nuo 0 iki 10
+                    studentai.push_back(student);
                 }
-                student.egz = std::rand() % 11; // Generuojame egzamino bala nuo 0 iki 10
-                studentai.push_back(student);
+            } catch (const std::bad_alloc& e) {
+                cout << "Nepavyko sukurti studentu: " << e.what() << endl;
+                return 1;
             }
         } else if (pasirinkimas == 'i') {
             ivestiStudentus(studentai);
